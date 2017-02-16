77e4eeed774e921cc7de7fe84b3f51dfd3301a3b
adubicki <addubicki@hotmail.com>
2017-02-16 15:55:20 -0800

    Added the battlesnake skeleton

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def static(path):
 
 @bottle.get('/')
 def index():
-    head_url = '%s://%s/static/head.jpg' % (
+    head_url = '%s://%s/static/head.png' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
