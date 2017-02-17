3786bcf17fe571c5b0fcf97d8c8caee5c8532e27
adubicki <addubicki@hotmail.com>
2017-02-16 16:07:13 -0800

    Added the battlesnake skeleton

--- app/main.py
+++ app/main.py
@@ -1,60 +1,66 @@
 import bottle
 import os
 import random
+from Board import Board
 
 
 @bottle.route('/static/<path:path>')
 def static(path):
-    return bottle.static_file(path, root='static/')
+	return bottle.static_file(path, root='static/')
 
 @bottle.get('/')
 def index():
-    head_url = '%s://%s/static/head.png' % (
-        bottle.request.urlparts.scheme,
-        bottle.request.urlparts.netloc
-    )
+	head_url = '%s://%s/static/head.png' % (
+		bottle.request.urlparts.scheme,
+		bottle.request.urlparts.netloc
+	)
 
-    return {
-        'color': '#00ff00',
-        'head': head_url
-    }
+	return {
+		'color': '#00ff00',
+		'head': head_url
+	}
 
 @bottle.post('/start')
 def start():
-    data = bottle.request.json
-    game_id = data['game_id']
-    board_width = data['width']
-    board_height = data['height']
+	data = bottle.request.json
+	game_id = data['game_id']
+	board_width = data['width']
+	board_height = data['height']
 
-    head_url = '%s://%s/static/head.png' % (
-        bottle.request.urlparts.scheme,
-        bottle.request.urlparts.netloc
-    )
+	head_url = '%s://%s/static/head.png' % (
+		bottle.request.urlparts.scheme,
+		bottle.request.urlparts.netloc
+	)
 
-    # TODO: Do things with data
+	# TODO: Do things with data
 
-    return {
-        'color': '#00FF00',
-        'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
-        'head_url': head_url,
-        'name': 'battlesnake-python'
-    }
+	return {
+		'color': '#00FF00',
+		'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
+		'head_url': head_url,
+		'name': 'battlesnake-python'
+	}
 
 
 @bottle.post('/move')
 def move():
-    data = bottle.request.json
+	data = bottle.request.json
+	board = Board(data['width'],data['height'])
+	print(board.toString())
+	print()
 
-    # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
+	# TODO: Do things with data
+	directions = ['up', 'down', 'left', 'right']
 
-    return {
-        'move': random.choice(directions),
-        'taunt': 'battlesnake-python!'
-    }
+	return {
+		'move': random.choice(directions),
+		'taunt': 'battlesnake-python!',
+		'data': data,
+		'board': board.toString()
+	}
 
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+	bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
